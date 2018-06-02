0bbe6ce2b339726f6f58f29b3a57c7d7e86aa38b
DavidWang <a394027456@163.com>
2018-06-02 14:59:16 +0800

    Calculate escape.

--- helloworld.pas
+++ helloworld.pas
@@ -2,13 +2,13 @@ program test;
 var
   x : Real;
   y : Integer;
-
+  z : Integer;
 procedure Proc(var x : Integer);
 begin
   x := 0;
 end;
 
-function Fun(x : Real) : Real;
+function Fun(x : Real; var z:Integer) : Real;
 var
   y : Real;
 begin
@@ -17,7 +17,7 @@ end;
 
 begin
   x := 1.0;
-  x := Fun(x);
+  x := Fun(x, z);
   Proc(y);
 
 end.
